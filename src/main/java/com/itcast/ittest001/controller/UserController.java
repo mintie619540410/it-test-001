4fdee357111004a6acf71fd2553bf3875a4b838d
mintie <619540410@qq.com>
2023-04-11 11:25:30 +0800

    master user edit

--- src/main/java/com/itcast/ittest001/controller/UserController.java
+++ src/main/java/com/itcast/ittest001/controller/UserController.java
@@ -6,4 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    public void test(){
+        System.out.println("hello git");
+    }
 }
